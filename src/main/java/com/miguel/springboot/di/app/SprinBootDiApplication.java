eb3bb2a5a0df2fe408cc951f0d0e8ec72ac8b9aa
miguel angel reyes <mareyes46@misena.edu.co>
2023-03-03 15:47:44 -0500

    first commit

---
+++ src/main/java/com/miguel/springboot/di/app/SprinBootDiApplication.java
@@ -0,0 +1,13 @@
+package com.miguel.springboot.di.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinBootDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinBootDiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/miguel/springboot/di/app/SprinBootDiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.miguel.springboot.di.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprinBootDiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
